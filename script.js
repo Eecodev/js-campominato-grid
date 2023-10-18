5deca777f5749a976ddfc84e1747b4db43eb936a
Eecodev <mnasser.bbk@gmail.com>
2023-10-18 15:35:56 +0200

    function created

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const btn = document.querySelector('.button');
 
 btn.addEventListener('click', function(){
-    //numero quadratini da generare
+    //numero squares da generare
     const numSquare = 100;
     //prendo la griglia di gioco
     const playground = document.getElementById('playground');
-    //ciclo per stampare i quadratini
+    //ciclo per stampare gli squares
     for(let i = 0; i >= numSquare; i++){
     }
 });
+
+function drawSquare(){
+    //creo nuovo elemento div
+    const square = document.createElement('div');
+    //aggiungo square 
+    square.classList.add('square');
+}
