4480d63b4a5bf611cb88b290d80b0906a90cb76a
Eecodev <mnasser.bbk@gmail.com>
2023-10-18 15:31:43 +0200

    added loop to print squares

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const btn = document.querySelector('.button');
 btn.addEventListener('click', function(){
     //numero quadratini da generare
     const numSquare = 100;
-
+    //prendo la griglia di gioco
+    const playground = document.getElementById('playground');
+    //ciclo per stampare i quadratini
+    for(let i = 0; i >= numSquare; i++){
+    }
 });
