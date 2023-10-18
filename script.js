41ee402cff3766793ab08e0a5dca5e7007a77b8f
Eecodev <mnasser.bbk@gmail.com>
2023-10-18 15:59:13 +0200

    initilised my function within my for loop

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-const btn = document.querySelector('.button');
+const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', () =>{
     //numero squares da generare
     const numSquare = 100;
     //prendo la griglia di gioco
     const playground = document.getElementById('playground');
+    //prima di riempire svuoto tutto
+    playground.innerHTML = '';
     //ciclo per stampare gli squares
-    for(let i = 0; i >= numSquare; i++){
-        
+    for(let i = 0; i < numSquare; i++){
+        //genero square
+        let square = drawSquare(i,numSquare);
+        console.log(square);
+        //appendo square al playground
+        playground.append(square);
+
     }
 });
 
 function drawSquare(squareIndex,numSquare){
+
     //creo nuovo elemento div
     const square = document.createElement('div');
     //aggiungo square 
     square.classList.add('square');
 
-    square.innerHTML(squareIndex + 1);
+    //stampo squares in html
+    square.innerHTML = squareIndex + 1;
+    //al click...
     square.addEventListener('click', function(){
+        //lo square diventa aquamarine
         square.classList.add('active');
+        //e il numero nero
         square.style.color = 'black';
-    })
+    });
+    return square;
 }
